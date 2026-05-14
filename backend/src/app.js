10a210fa68b1de34e90945a48af1ba590f71c031
Aaditya Ashwin Rao <aadityaashwinrao@gmail.com>
2026-05-14 22:48:34 +0530

    Added create Ebook endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { query } from './config/db.js';
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.routes.js';
+import bookRoutes from './routes/book.routes.js';
 const app = express();
 
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
+app.use("/api/ebook",bookRoutes);
 app.get('/test-db', async (req, res) => {
   try {
     const result = await query('SELECT NOW()');
